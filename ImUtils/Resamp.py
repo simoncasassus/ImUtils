83f073fea67a197bba80c83d95f5f9e86c106402
Simon Casassus <simoncasassus@gmail.com>
2024-02-25 21:09:58 +0000

    sync

--- ImUtils/Resamp.py
+++ ImUtils/Resamp.py
@@ -10,150 +10,147 @@ from pprint import pprint
 
 import ImUtils.Cube2Im as Cube2Im
 
+
 def loadfits(namefile):
 
-    hdus=fits.open(namefile)
-    if (isinstance(hdus,list)):
-        hdu=hdus[0]
+    hdus = fits.open(namefile)
+    if (isinstance(hdus, list)):
+        hdu = hdus[0]
     else:
-        hdu=hdus
+        hdu = hdus
     datacube = hdu.data
     hdr = hdu.header
-    
+
     return datacube, hdr
 
 
-
-
-
-def gridding(arg1, imagefile_2,fileout=False,fullWCS=True,ReturnHDU=False,ReturnHDUList=False,order=1,Verbose=False,mode='constant'):
+def gridding(arg1,
+             imagefile_2,
+             fileout=False,
+             fullWCS=True,
+             ReturnHDU=False,
+             ReturnHDUList=False,
+             order=1,
+             Verbose=False,
+             mode='constant'):
     """
     Interpolates Using ndimage and astropy.wcs for coordinate system.
     arg1 is the input data to be gridded, can be either a fits filename or an HDU
     arg2 contains the reference hdr, can either be a fits filename or a header
     """
     if (ReturnHDUList):
-        ReturnHDU=True
-    
-    if (isinstance(arg1,str)):
+        ReturnHDU = True
+
+    if (isinstance(arg1, str)):
         im1, hdr1 = loadfits(arg1)
-    elif (isinstance(arg1,fits.hdu.image.PrimaryHDU)):
+    elif (isinstance(arg1, fits.hdu.image.PrimaryHDU)):
         im1 = arg1.data
         hdr1 = arg1.header
-    elif (isinstance(arg1,fits.hdu.hdulist.HDUList)):
+    elif (isinstance(arg1, fits.hdu.hdulist.HDUList)):
         im1 = arg1[0].data
         hdr1 = arg1[0].header
     else:
         sys.exit("not an recognized input format")
-        
 
-    IsCube=False
+    IsCube = False
     if (len(im1.shape) > 2):
         if (len(im1.shape) > 3):
-            im1=im1[0,:,:,:]
-        if (im1.shape[2]>1):
-            IsCube=True
+            im1 = im1[0, :, :, :]
+        if (im1.shape[2] > 1):
+            IsCube = True
         else:
-            im1=im1[0,:,:]
+            im1 = im1[0, :, :]
 
-            
-
-        
-    if (isinstance(imagefile_2,str)):
+    if (isinstance(imagefile_2, str)):
         im2, hdr2 = loadfits(imagefile_2)
     else:
-        hdr2=imagefile_2
+        hdr2 = imagefile_2
 
-    SetCRVAL3_1=False
-    
+    SetCRVAL3_1 = False
+
     if ('CRVAL3' in hdr1.keys()):
-        SetCRVAL3_1=True
-        hdr1_CRVAL3=hdr1['CRVAL3']
-    SetCRVAL3_2=False
+        SetCRVAL3_1 = True
+        hdr1_CRVAL3 = hdr1['CRVAL3']
+    SetCRVAL3_2 = False
     if ('CRVAL3' in hdr2.keys()):
-        SetCRVAL3_2=True
-        hdr2_CRVAL3=hdr2['CRVAL3']
-        
-    #hdr1.pop('CRVAL3', None)  
-    #hdr2.pop('CRVAL3', None)  
-    
-    hdr1im=Cube2Im.trimhead(hdr1,Inplace=False)
-    hdr2im=Cube2Im.trimhead(hdr2,Inplace=False)
+        SetCRVAL3_2 = True
+        hdr2_CRVAL3 = hdr2['CRVAL3']
+
+    #hdr1.pop('CRVAL3', None)
+    #hdr2.pop('CRVAL3', None)
+
+    hdr1im = Cube2Im.trimhead(hdr1, Inplace=False)
+    hdr2im = Cube2Im.trimhead(hdr2, Inplace=False)
 
     if Verbose:
         pprint(hdr2im)
-    
+
     w1 = WCS(hdr1im)
     w2 = WCS(hdr2im)
 
-    
-    
     n2x = hdr2['NAXIS1']
     n2y = hdr2['NAXIS2']
-    k2s=np.arange(0,n2x)
-    l2s=np.arange(0,n2y)
+    k2s = np.arange(0, n2x)
+    l2s = np.arange(0, n2y)
     kk2s, ll2s = np.meshgrid(k2s, l2s)
 
     if (fullWCS):
         xxs2wcs, yys2wcs = w2.all_pix2world(kk2s, ll2s, 0)
-        kk1s, ll1s = w1.all_world2pix(xxs2wcs,yys2wcs,0,tolerance=1e-12)
+        kk1s, ll1s = w1.all_world2pix(xxs2wcs, yys2wcs, 0, tolerance=1e-12)
     else:
         xxs2wcs, yys2wcs = w2.wcs_pix2world(kk2s, ll2s, 0)
-        kk1s, ll1s = w1.wcs_world2pix(xxs2wcs,yys2wcs,0)
+        kk1s, ll1s = w1.wcs_world2pix(xxs2wcs, yys2wcs, 0)
 
+    im1 = np.nan_to_num(im1)
 
-    im1=np.nan_to_num(im1)
-    
     if IsCube:
-        print("im1.shape",im1.shape)
-        nk=im1.shape[0]
-        resamp=np.zeros((nk,n2y,n2x))
-        print("Resampling ... nk=",nk)
+        print("im1.shape", im1.shape)
+        nk = im1.shape[0]
+        resamp = np.zeros((nk, n2y, n2x))
+        print("Resampling ... nk=", nk)
         for k in list(range(nk)):
-            print(" k= ",k)
-            aplane=im1[k,:,:]
-            resamp_aplane = map_coordinates(aplane, [ll1s, kk1s],prefilter=False,order=order,mode=mode) #,order=1
-            resamp[k,:,:]=resamp_aplane
+            print(" k= ", k)
+            aplane = im1[k, :, :]
+            resamp_aplane = map_coordinates(aplane, [ll1s, kk1s],
+                                            prefilter=False,
+                                            order=order,
+                                            mode=mode)  #,order=1
+            resamp[k, :, :] = resamp_aplane
 
     else:
-        resamp = map_coordinates(im1, [ll1s, kk1s],prefilter=False,order=order,mode=mode) #,order=1
+        resamp = map_coordinates(im1, [ll1s, kk1s],
+                                 prefilter=False,
+                                 order=order,
+                                 mode=mode)  #,order=1
 
-    
-    resamp=np.nan_to_num(resamp)
+    resamp = np.nan_to_num(resamp)
 
     #if (SetCRVAL3_1):
     #    hdr1['CRVAL3']=hdr1_CRVAL3
     if (SetCRVAL3_2):
-        hdr2['CRVAL3']=hdr2_CRVAL3
+        hdr2['CRVAL3'] = hdr2_CRVAL3
 
     if IsCube:
         #hdr2.pop('NAXIS3',None)
-        hdr2.pop('EXTEND',None)
-        
-        #hdr2['NAXIS3']=hdr1['NAXIS3']
-        hdr2['CRPIX3']=hdr1['CRPIX3']
-        hdr2['CDELT3']=hdr1['CDELT3']
-        hdr2['CRVAL3']=hdr1['CRVAL3']
-        
-    if (fileout):
-        
-        fits.writeto(fileout,resamp, hdr2, overwrite=True)
+        hdr2.pop('EXTEND', None)
 
+        #hdr2['NAXIS3']=hdr1['NAXIS3']
+        hdr2['CRPIX3'] = hdr1['CRPIX3']
+        hdr2['CDELT3'] = hdr1['CDELT3']
+        hdr2['CRVAL3'] = hdr1['CRVAL3']
+
+    if (fileout):
+
+        fits.writeto(fileout, resamp, hdr2, overwrite=True)
 
     if ReturnHDU:
-        rethdu=fits.PrimaryHDU()
-        rethdu.data=resamp
-        rethdu.header=hdr2
-        if ReturnHDUList:  
+        rethdu = fits.PrimaryHDU()
+        rethdu.data = resamp
+        rethdu.header = hdr2
+        if ReturnHDUList:
             hdul = fits.HDUList([rethdu])
             return hdul
         else:
             return rethdu
     else:
         return resamp
-
-
-    
-
-
-
